ed29cd72b5a756272755194115f7a66cbd961fba
serhii.hychka <sergij41@gmail.com>
2014-06-10 21:15:48 +0300

    Add huylos photo Add vacancies, CV pages for student Add loremIpsum property to user bean Add company side menu

--- web/src/main/java/edu/system/employment/controller/UserBean.java
+++ web/src/main/java/edu/system/employment/controller/UserBean.java
@@ -19,7 +19,11 @@ public class UserBean {
 	private BaseDaoBean dao;
 	 
 	private Student student;
-	
+
+    private String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
+            "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
 	public Student getStudent() {
 		return student;
 	}
@@ -28,7 +32,15 @@ public class UserBean {
 		this.student = student;
 	}
 
-	public String register(){
+    public String getLoremIpsum() {
+        return loremIpsum;
+    }
+
+    public void setLoremIpsum(String loremIpsum) {
+        this.loremIpsum = loremIpsum;
+    }
+
+    public String register(){
 		dao.create(student);
 		return "index";
 	}
